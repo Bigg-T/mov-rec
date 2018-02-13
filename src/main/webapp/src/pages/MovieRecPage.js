22109f34c98909a187b092e3ed5b6d74fb2d35d0
Thien Nguyen <nguyen.thie@husky.neu.edu>
2018-02-13 15:57:12 -0500

    Refolder component, segment out MovieRecPage

--- src/main/webapp/src/pages/MovieRecPage.js
+++ src/main/webapp/src/pages/MovieRecPage.js
@@ -3,9 +3,8 @@
  */
 import React, {Component} from 'react';
 import {Grid, Header, Container, Card} from 'semantic-ui-react';
-import MovieCard from '../components/MovieCard';
 import '../css/MovieRecPage.css';
-import axios from 'axios';
+import MovieSection from '../components/MovieSection';
 
 class MovieRecPage extends Component {
 
@@ -14,13 +13,13 @@ class MovieRecPage extends Component {
         <div>
           <div className="mysection">
             <Container>
-              <MovieSection sectionName={"What's Hot"}/>
+              <MovieSection sectionName={"What's Hot"} perRow={2} perCol={3}/>
             </Container>
             <Container>
-              <MovieSection sectionName={"Action"}/>
+              <MovieSection sectionName={"Action"} perRow={2} perCol={3} />
             </Container>
             <Container>
-              <MovieSection sectionName={"Comedy"}/>
+              <MovieSection sectionName={"Comedy"} perRow={2} perCol={3} />
             </Container>
           </div>
         </div>
@@ -28,56 +27,6 @@ class MovieRecPage extends Component {
   }
 }
 
-class MovieSection extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      myData : [],
-    }
-  }
 
-  componentDidMount() {
-    const UM = "https://api.themoviedb.org/3/movie/201/lists?api_key=492a79d4999e65c2324dc924891cb137&language=en-US&page=1";
-    const UM2 = 'https://api.themoviedb.org/3/movie/popular?api_key=492a79d4999e65c2324dc924891cb137&language=en-US&page=1';
-    const MDB_API_KEY = '492a79d4999e65c2324dc924891cb137';
-    const BASE_MDB_URL = 'https://api.themoviedb.org/3/';
-    let URL = BASE_MDB_URL+'/movie/popular?api_key='+MDB_API_KEY + '&language=en-US&page=1';
-
-    axios.get(UM2)
-    .then(res => {
-      const data = res.data;
-      console.log('MyData');
-      console.log(data.results);
-      this.setState({myData : data.results });
-    })
-  }
-
-  renderRow(cardNum) {
-    let names = this.state.myData.map(obj => obj.title);
-    let row = [];
-    // console.log(row);
-    for (let i = 0; i < cardNum; i++) {
-      row.push(<MovieCard key={i} movieName={names[i]/* Movie name*/}/>)
-    }
-    console.log(this.state.myData[0]);
-    // console.log(NODE);
-    console.log(this.state.myData);
-    return row;
-  }
-
-  render() {
-    return (
-        <div >
-          <div className="myheader">
-            <Header content={this.props.sectionName} as='h1'/>
-          </div>
-          <Card.Group itemsPerRow={3}>
-            {this.renderRow(6)}
-          </Card.Group>
-        </div>
-    );
-  }
-
-}
 
 export default MovieRecPage;
