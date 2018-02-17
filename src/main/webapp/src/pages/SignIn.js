03647f2265857bf1c464da7ab074e0673f7ba089
Thien Nguyen <nguyen.thie@husky.neu.edu>
2018-02-17 00:15:48 -0500

    TEAM31-10 conected with backend for verifying (user/pass) logged in

--- src/main/webapp/src/pages/SignIn.js
+++ src/main/webapp/src/pages/SignIn.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {Container, Form, Header } from 'semantic-ui-react';
-
-
+import axios from 'axios';
 
 class SignIn extends Component {
   constructor(props) {
@@ -11,32 +10,53 @@ class SignIn extends Component {
       password: '',
       submittedUserName: '',
       submittedPassword: ''};
+      isLoggedin: false;
+      errorMsg: "";
   }
 
   handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
   handleSubmit() {
     const { username, password } = this.state;
-
     //should be doing post to backend
-    this.setState({ submittedUserName: username, submittedPassword: password })
+    // somehow this does not get updated in time before it git axios
+    this.setState({ submittedUserName: username, submittedPassword: password });
+    let URL = "http://localhost:8081/api/user/add_User/?";
+    let fname = "fname=bigt2&";
+    let lname = "lname=mofo&";
+    let email = "email=bigt2@mofo.com&";
+    let pw = "pw=mypass&username=bigtmofo2";
+    let URL2 = "http://localhost:8081/api/user/validate_login/?";
+    let un = "username=" + this.state.username;
+    let pass = "pw=" + this.state.password;
+    console.log("UserName:");
+    console.log(this.state.username);
+    axios.get(URL2+ un+'&'+pass)
+    .then((code) => {
+      console.log(code);
+      this.setState({isLoggedin : code.data.isSuccess});
+      // console.log("Status of account logged in:" + code.data.isSuccess);
+      let isSucess = code.data.isSuccess;
+      if (isSucess) {
+        window.location.href = '/';// change the history path, to reroute (hack)
+        window.localStorage['isLoggedIn'] = true;
+      } else {
+        this.setState({errorMsg : "You incorrect password or username."})
+      }
+    });
   }
 
   render() {
-
-
     const username = this.state.username;
     const password = this.state.password;
     const submittedUserName = this.state.submittedUserName;
     const submittedPassword = this.state.submittedPassword;
 
-
-
     return (
         <Container>
-
+          <div>{this.state.errorMsg}</div>
           <Form onSubmit={() => this.handleSubmit()}>
-<Header as='h2'>Sign In</Header>
+            <Header as='h2'>Sign In</Header>
             <Form.Group widths="3">
               <Form.Input fluid required
                           label="User Name" placeholder='username' name='username'
@@ -52,6 +72,8 @@ class SignIn extends Component {
 
 
           </Form>
+          {/*<div>{Object.keys(this.state).map(key => key + " : " + this.state[key] + "\n")}</div>*/}
+          {/*<p>{this.state.isLoggedin}</p>*/}
         </Container>
     );
   }
--- src/main/webapp/src/utils/MovieHallConstant.js
+++ src/main/webapp/src/utils/MovieHallConstant.js
@@ -1,3 +1,7 @@
 /**
  * Created by t on 2/16/18.
  */
+
+export const MH_BASE_URL = "http://localhost:8081/api/";
+
+const test = "user/add_User/?fname=bigt&lname=mofo&email=bigt@mofo.com&pw=mypass&username=bigtmofo";
