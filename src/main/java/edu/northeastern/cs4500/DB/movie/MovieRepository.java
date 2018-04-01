4bb484fd9c8fc1b09b980bb0aaf939a14e7b078c
Jean Paul Torre <torre.j@husky.neu.edu>
2018-04-01 06:23:14 -0400

    updated object reference

--- src/main/java/edu/northeastern/cs4500/DB/movie/MovieRepository.java
+++ src/main/java/edu/northeastern/cs4500/DB/movie/MovieRepository.java
@@ -3,7 +3,9 @@ package edu.northeastern.cs4500.DB.movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import edu.northeastern.cs4500.models.MovieRatingsObject;
+
 @Repository
-public interface MovieRepository extends JpaRepository<MovieObject, Integer>{
+public interface MovieRepository extends JpaRepository<MovieRatingsObject, Integer>{
 
 }
